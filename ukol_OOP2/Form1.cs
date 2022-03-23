4bd4f8aa01829c76da1c3979212975ab44749bc8
Filip Vidlák <vidlakf.03@spst.eu>
2022-03-23 15:49:12 +0100

    Přidejte soubory projektu.

---
+++ ukol_OOP2/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ukol_OOP2
+{
+    public partial class Form1 : Form
+    {
+        NakladniAuto konstruktor;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                konstruktor = new NakladniAuto(textBox1.Text, Int32.Parse(textBox2.Text));
+                button1.Enabled = false;
+                button2.Enabled = true;
+                button3.Enabled = true;
+                button4.Enabled = true;
+            }
+            catch
+            {
+                MessageBox.Show("Zadej hodnoty podle zadání!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                konstruktor.NalozNaklad(int.Parse(textBox3.Text));
+
+            }
+            catch
+            {
+                MessageBox.Show("Zadej hodnoty podle zadání!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                konstruktor.VylozNaklad(int.Parse(textBox3.Text));
+            }
+            catch
+            {
+                MessageBox.Show("Zadej hodnoty podle zadání!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(konstruktor.ToString());
+        }
+    }
+}
---
+++ ukol_OOP2/NakladniAuto.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ukol_OOP2
+{
+    public class NakladniAuto
+    {
+        private string spz;
+        private int nosnost;
+        private int naklad = 0;
+        public int hmotnostnakladu { get { return naklad; } }
+
+        public NakladniAuto(string spz, int nosnost)
+        {
+            this.spz = spz;
+            this.nosnost = nosnost;
+        }
+       
+        public void NalozNaklad(int hmotnostnakladu)
+        {
+            if (hmotnostnakladu > nosnost)
+            {
+                naklad = nosnost;
+                MessageBox.Show("Naloženo bylo " + hmotnostnakladu + " tun. Přebytek je " + (hmotnostnakladu - naklad) + " tun.");
+            }
+            else
+            {
+                naklad = hmotnostnakladu;
+                MessageBox.Show("Náklad se úspěšně naložil.");
+            }
+        }
+
+        public void VylozNaklad(int hmotnostnakladu)
+        {
+            if (hmotnostnakladu > naklad) {
+                MessageBox.Show("Náklad byl vyložen, ale chybí " + (hmotnostnakladu - naklad) + " tun.");
+                naklad = 0;
+            }
+            else {
+                MessageBox.Show("Náklad byl úspěšně vyložen.");
+                naklad = 0;
+            }
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Nákladní auto {0} má nosnost {1} tun a má naloženo {2} tun nákladu.", spz, nosnost, naklad);
+        }
+    }
+}
+
